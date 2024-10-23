5969c393acc93cbd0690a53668ff2a01567f27c6
Pushkar Singh <84075300+PushkarOP@users.noreply.github.com>
2024-10-23 14:04:49 +0530

    Update auth.ts

--- src/constants/auth.ts
+++ src/constants/auth.ts
@@ -1,4 +1,4 @@
-export const officialAPIEndpoint = 'https://rural-heath-nexusai4349-d414845a.koyeb.app/nexus/v1/chat/completions';
+export const officialAPIEndpoint = 'https://api.nexusapi.tech/nexus/v1/chat/completions';
 export const defaultAPIEndpoint =
   import.meta.env.VITE_DEFAULT_API_ENDPOINT || officialAPIEndpoint;
 
