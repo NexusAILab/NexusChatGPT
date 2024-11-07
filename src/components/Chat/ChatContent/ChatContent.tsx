c2c9ac8ba2b2c2030668bf0e4ee7e74185964826
Pushkar Singh <84075300+PushkarOP@users.noreply.github.com>
2024-11-07 09:04:09 +0530

    Update ChatContent.tsx

--- src/components/Chat/ChatContent/ChatContent.tsx
+++ src/components/Chat/ChatContent/ChatContent.tsx
@@ -110,13 +110,13 @@ const ChatContent = () => {
           >
             {useStore.getState().generating || (
               <div className="md:w-[calc(100%-50px)] flex gap-4 flex-wrap justify-center mt-4">
-                {/* Captcha */}
+                {/* Buttons */}
                 <div
                   className="cf-turnstile"
                   data-sitekey="0x4AAAAAAAzRsaZd0P9-qFot"
+                  data-theme="light"
+                  data-callback="onTurnstileSuccess"
                 ></div>
-
-                {/* Buttons */}
                 <DownloadChat saveRef={saveRef} />
                 <ShareGPT />
                 <CloneChat />
