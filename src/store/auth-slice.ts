12dc5cfb8e7ad7768b27a60f535d04d1f2255257
Opnoice <78496510+opnoice@users.noreply.github.com>
2024-11-10 18:46:07 +0530

    Update auth-slice.ts

--- src/store/auth-slice.ts
+++ src/store/auth-slice.ts
@@ -5,18 +5,15 @@ export interface AuthSlice {
   apiKey?: string;
   apiEndpoint: string;
   firstVisit: boolean;
-  turnstileToken: string | null;
   setApiKey: (apiKey: string) => void;
   setApiEndpoint: (apiEndpoint: string) => void;
   setFirstVisit: (firstVisit: boolean) => void;
-  setTurnstileToken: (token: string | null) => void;
 }
 
 export const createAuthSlice: StoreSlice<AuthSlice> = (set, get) => ({
   apiKey: import.meta.env.VITE_OPENAI_API_KEY || undefined,
   apiEndpoint: defaultAPIEndpoint,
   firstVisit: true,
-  turnstileToken: null,
   setApiKey: (apiKey: string) => {
     set((prev: AuthSlice) => ({
       ...prev,
@@ -33,11 +30,6 @@ export const createAuthSlice: StoreSlice<AuthSlice> = (set, get) => ({
     set((prev: AuthSlice) => ({
       ...prev,
       firstVisit: firstVisit,
-    }));
-  },
-  setTurnstileToken: (token) => {
-    set((state) => ({
-      turnstileToken: token,
     }));
   },
 });
