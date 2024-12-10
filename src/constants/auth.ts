a70550fd42ed8f258675b0b2aa48414b291fc98f
Opnoice <78496510+opnoice@users.noreply.github.com>
2024-12-10 14:29:50 +0530

    Update auth.ts

--- src/constants/auth.ts
+++ src/constants/auth.ts
@@ -1,4 +1,4 @@
-export const officialAPIEndpoint = 'https://api.nexusapi.tech/nexus/v1/chat/completions';
+export const officialAPIEndpoint = 'https://api.nexusmind.tech/nexus/v1/chat/completions';
 export const defaultAPIEndpoint =
   import.meta.env.VITE_DEFAULT_API_ENDPOINT || officialAPIEndpoint;
 
